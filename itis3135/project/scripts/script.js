ac8c0458f19ef7a15d36241745df6a3da4ce0956
QuietNook <sougo13.ad@gmail.com>
2024-11-16 12:08:20 -0500

    project push

---
+++ itis3135/project/scripts/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Map of game identifiers to their embed code
+    const gameEmbeds = {
+        "teen-titans": `<iframe src="https://archive.org/embed/teen-titans-battle-blitz-game" width="560" height="384" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`,
+        "samurai-jack": `<iframe src="https://archive.org/embed/2675-samurai-jack-code-of-the-samurai" width="560" height="384" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`,
+        "bullet-time": `<embed src="https://www.twoplayergames.org/embed/bullet-time-fighting" width="800" height="600" />`,
+        "super-smash": `<iframe src="https://archive.org/embed/supersmashflash_swf" width="560" height="384" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`,
+        "bloons-td": `<iframe src="https://archive.org/embed/btd1_20220404" width="560" height="384" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`,
+    };
+
+    // Reference the game container
+    const gameContainer = document.getElementById("game-container");
+
+    // Add click event listeners to all buttons in the game list
+    const gameButtons = document.querySelectorAll("#game-list button");
+    gameButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Get the selected game identifier from the data attribute
+            const gameKey = button.getAttribute("data-game");
+            // Load the corresponding embed code into the game container
+            gameContainer.innerHTML = gameEmbeds[gameKey] || "<p>Game not found.</p>";
+        });
+    });
+});
